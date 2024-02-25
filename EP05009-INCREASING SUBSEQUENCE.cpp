f557c21b85691999063c50c4aeecc61f461b54e1
HieuNM <154902053+HieuNguyenMinh1804@users.noreply.github.com>
2024-02-25 19:33:57 +0700

    Add files via upload

---
+++ EP05009-INCREASING SUBSEQUENCE.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  a[21], b[21], n;
+set <string> s;
+void Try(int i){
+	for(int j = 0; j <= 1; ++j){
+		b[i] = j;
+		if(i == n-1){
+			
+			string p = "";
+			vector<int> tmp;
+			for(int idx = 0; idx < n; ++idx)
+				if(b[idx]) tmp.push_back(a[idx]);
+			if(tmp.size() >= 2){
+				int ok = 1;
+				for(int idx = 0 ; idx < tmp.size()-1; ++idx){
+					if(tmp[idx] > tmp[idx+1]){
+						ok = 0;
+						break;
+					}
+				}
+				if(ok){
+//					for(int idx = 0 ; idx < tmp.size(); ++idx){
+//						cout << tmp[idx] << " ";
+//					}
+//					cout << endl;
+					for(int idx = 0 ; idx < tmp.size(); ++idx){
+						p += to_string(tmp[idx]);
+						p += " ";
+					}
+					s.insert(p);
+				}
+			}		
+		}
+		else Try(i+1);
+	}
+}
+int main(){
+	freopen("SEQUENCE.in", "r", stdin);
+	cin >> n;
+	for(int i = 0; i < n; ++i) cin >> a[i];
+	Try(0);
+	while(s.size()){
+		string x = *s.begin();
+		cout << x << endl;
+		s.erase(x);
+	}
+}
