dbd46f41f805876572f64d786b0ecbbc736ec991
HieuNM <154902053+HieuNguyenMinh1804@users.noreply.github.com>
2024-02-26 01:15:10 +0700

    Update EP06015-AREA OF A POLYGON.cpp

--- EP06015-AREA OF A POLYGON.cpp
+++ EP06015-AREA OF A POLYGON.cpp
@@ -1,18 +1,19 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define endl "\n"
-#define fast(); ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 using namespace std;
 struct p{
 	double x, y;	
 };
 int main(){
-	fast();
 	int t; cin >> t;
 	while(t--){
 		int n; cin >> n;
 		p a[n];
 		for(int i = 0; i < n; ++i) cin >> a[i].x >> a[i].y;
-		
+		double res1 = 0, res2 = 0;
+		for(int i = 0; i < n-1; ++i) res1 += a[i].x*a[i+1].y;
+		res1 += a[n-1].x*a[0].y;		
+		for(int i = 0; i < n-1; ++i) res2 += a[i].y*a[i+1].x;
+		res2 += a[n-1].y*a[0].x;
+		printf("%.3lf\n",0.5*abs(res1-res2));
 	}
 }
